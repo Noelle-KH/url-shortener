0a45c76ce299af907767b717f77e7796c209b57f
Noelle-KH <noelletang86@gmail.com>
2022-11-20 02:59:58 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+
 const routes = require('./routes')
-
-if (process.env.NODE_URL !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URL)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')  
 
 const app = express()
 const port = 3000
--- models/seeds/url-seeders.js
+++ models/seeds/url-seeders.js
@@ -1,21 +1,8 @@
-const mongoose = require('mongoose')
+const db = require('../../config/mongoose')
 const Url = require('../url-model')
 const urlData = require('./url-data.json')
 
-if (process.env.NODE_URL !== 'production') {
-  require('dotenv').config()
-}
-
-mongoose.connect(process.env.MONGODB_URL)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
 db.once('open', () => {
-  console.log('mongodb connected!')
   Url.insertMany(urlData)
   console.log('done')
 })
